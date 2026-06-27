74cd8067e15270d5ec51f1b9c229119224c4f9fc
elajmedoune <gningue7@icloud.com>
2026-06-27 22:09:04 +0000

    fix: register SW manually

--- vite.config.js
+++ vite.config.js
@@ -1,39 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      workbox: {
-        globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2}'],
-        runtimeCaching: [
-          {
-            urlPattern: /^https:\/\/.*\.supabase\.co\/.*/i,
-            handler: 'NetworkFirst',
-            options: {
-              cacheName: 'supabase-cache',
-              expiration: { maxEntries: 50, maxAgeSeconds: 300 },
-            },
-          },
-        ],
-      },
-      manifest: {
-        name: 'TradeForge',
-        short_name: 'TradeForge',
-        description: 'Journal de trading avancé',
-        theme_color: '#F7B731',
-        background_color: '#070A0F',
-        display: 'standalone',
-        icons: [
-          { src: '/icon-192.png', sizes: '192x192', type: 'image/png' },
-          { src: '/icon-512.png', sizes: '512x512', type: 'image/png' },
-        ],
-      },
-    }),
-  ],
+  plugins: [react()],
   build: {
     sourcemap: false,
     rollupOptions: {
